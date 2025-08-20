66c2822a1623192a805231eba742e9db484cc5e1
Onyekachi Iloghalu <kachi.iloghalu@gmail.com>
2025-08-19 19:56:06 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from streamlit_option_menu import option_menu
 # loading the saved models
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 heart_disease_model = pickle.load(open('hearth_model.sav', 'rb'))
-parkinson_model = pickle.load(open('parkinsons_model.sav','rb'))
+parkinson_model = pickle.load(open('parkinson_model.sav','rb'))
 
 
 
